00994ea456efe2af43625c560677ca4779941645
Luke Carl Hartman <luke@lukecarlhartman.com>
2022-09-26 09:15:28 -0700

    ecommerce revenue updates and bar graph

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import {
+	SparklineComponent,
+	Inject,
+	SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
+// implementing syncfusion: https://ej2.syncfusion.com/react/documentation/sparkline/getting-started/
+const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
+	return (
+		<SparklineComponent
+			id={id}
+			height={height}
+			width={width}
+			lineWidth={1}
+			valueType="Numeric"
+			fill={color}
+			border={{ color: currentColor, width: 2 }}
+			dataSource={data}
+			xName="x"
+			yName="y"
+			type={type}
+			tooltipSettings={{
+				visible: true,
+				// eslint-disable-next-line no-template-curly-in-string
+				format: "${x} : data ${yval}",
+				trackLineSettings: {
+					visible: true,
+				},
+			}}>
+			<Inject services={[SparklineTooltip]} />
+		</SparklineComponent>
+	);
+};
 
-const SparkLine = () => {
-    return (
-        <div>SparkLine</div>
-    )
-}
-
-export default SparkLine
+export default SparkLine;
