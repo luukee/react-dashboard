f8bbd4388fb262a608909ed2a5ec6209a4b21c38
Luke Carl Hartman <luke@lukecarlhartman.com>
2022-09-27 06:45:08 -0700

    customers page table add

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import {
+	GridComponent,
+	ColumnsDirective,
+	ColumnDirective,
+	Page,
+	Selection,
+	Inject,
+	Edit,
+	Toolbar,
+	Sort,
+	Filter,
+} from "@syncfusion/ej2-react-grids";
+
+import { customersData, customersGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Customer = () => {
-  return <div>Customer</div>;
+	return (
+		<div className="margin-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+			<Header
+				category="Page"
+				title="Customers"
+			/>
+			<GridComponent
+				id="gridcomp"
+				dataSource={customersData}
+				allowPaging
+				allowSorting
+				toolbar={["Search"]}
+				width="auto">
+				<ColumnsDirective>
+					{customersGrid.map((item, index) => (
+						<ColumnDirective
+							key={index}
+							{...item}
+						/>
+					))}
+				</ColumnsDirective>
+				<Inject services={[Page, Toolbar]} />
+			</GridComponent>
+		</div>
+	);
 };
 
 export default Customer;
